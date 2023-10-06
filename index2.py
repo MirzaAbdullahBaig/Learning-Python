6ea322d34e0e61d34270f341c9b59774e1e803a7
Mirza Abdullah Baig <abaig7921gmail.com>
2023-10-06 12:46:24 +0500

    Arithmetic operations Practice

---
+++ index2.py
@@ -0,0 +1,10 @@
+# Arithmetic operations
+
+print ("Sum:", 4 + 3)
+print ("Difference:", 4 - 3)
+print ("Product:", 4 * 3)
+print ("Division:", 4 / 3)
+print ("floor Division:", 4 // 3)
+print ("Remainder:", 4 % 3)
+print ("Exponentiation:", 4 ** 3)
+
