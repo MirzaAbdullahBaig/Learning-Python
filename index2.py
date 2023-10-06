76235a45551d78393378c90dc034e12bd136196b
Mirza Abdullah Baig <abaig7921gmail.com>
2023-10-06 18:04:41 +0500

    Arithmetic, Assignment & Comparison operaters

--- index2.py
+++ index2.py
@@ -25,3 +25,16 @@ print (num1, num2)
 
 num2 /= num1
 print (num1, num2)
+
+
+# Comparison operaters
+
+num1 = 4
+num2 = 5
+print (num1 == num2) #false
+print (num1 != num2) #true
+print (num1 > num2) #false
+print (num1 < num2) #true
+print (num1 >= num2) #false
+print (num1 <= num2) #true
+
