38c82f445b6f1f4f8cb8e0f353a7ffbce1baaffa
Mirza Abdullah Baig <abaig7921gmail.com>
2023-10-06 19:03:33 +0500

    Arithmetic, Assignment, Comparison & Logical operaters

--- index2.py
+++ index2.py
@@ -38,3 +38,13 @@ print (num1 < num2) #true
 print (num1 >= num2) #false
 print (num1 <= num2) #true
 
+
+# Logical operaters
+
+exp1 = 4 > 3
+exp2 = 4 < 3
+
+print("exp1 & exp2 with and operater", exp1 and exp2)
+print("exp1 & exp2 with or operater", exp1 or exp2)
+print("exp1 with not operater", not(exp1))
+print("exp2 with not operater", not(exp2))
