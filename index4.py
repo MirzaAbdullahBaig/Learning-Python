743bb41130214d8636f645efcf6437e88d7b6c4d
Mirza Abdullah Baig <abaig7921gmail.com>
2023-10-07 16:22:59 +0500

    TypeCasting Practice

---
+++ index4.py
@@ -0,0 +1,14 @@
+# TypeCasting Practice
+
+num1 = 2
+num2 = 4
+num3 = 6
+
+str_num1 = str(num1)
+str_num2 = str(num2)
+str_num3 = str(num3)
+
+final_str = int(str_num1 + str_num2 + str_num3)
+final_int = int(str_num1 + str_num2 + str_num3)
+
+print("The sum of num1, num2 and num3 is :", final_str, type(final_str))
