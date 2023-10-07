6d694a733d9edaea50689befdcb0ed8806c81ab0
Mirza Abdullah Baig <abaig7921gmail.com>
2023-10-07 16:24:22 +0500

    Basic Programs

---
+++ Write Programs.py
@@ -0,0 +1,8 @@
+# Write a program to calculate volume of sphere
+
+# Formula of volume calculation: (4/3) * 3.145 * (radius ** 3)
+
+radius = float(input("Enter radius of sphere: "))
+volume = (4/3) * 3.145 * (radius ** 3)
+
+print("Volume of sphere is: ", volume)
