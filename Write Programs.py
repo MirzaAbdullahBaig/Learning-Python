69e54ca97c147580f825815048b9b567925227c4
Mirza Abdullah Baig <abaig7921gmail.com>
2023-10-07 16:43:09 +0500

    Basic Programs

--- Write Programs.py
+++ Write Programs.py
@@ -6,3 +6,11 @@ radius = float(input("Enter radius of sphere: "))
 volume = (4/3) * 3.145 * (radius ** 3)
 
 print("Volume of sphere is: ", volume)
+
+
+# Write a program to convert temperature from celcius to fahrenheit
+
+temp_in_celcius = float(input("Enter temperature in celcius: "))
+fahrenheit = (temp_in_celcius * 9 / 5) + 32
+
+print ("Temperature in Fahrenheit: ", fahrenheit)
