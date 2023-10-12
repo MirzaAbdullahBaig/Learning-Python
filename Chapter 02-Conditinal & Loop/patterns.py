b2e55323d7b0e5b75b0db1e9b4db9a16de62de9d
Mirza Abdullah Baig <abaig7921gmail.com>
2023-10-12 12:01:22 +0500

    for and while Nested loop | Printing patterns

--- Chapter 02-Conditinal & Loop/patterns.py
+++ Chapter 02-Conditinal & Loop/patterns.py
@@ -31,3 +31,26 @@ for i in range(1, n + 1):
     for j in range(1, i + 1):
         print(chr(64 + j), end="")
     print()
+
+# practice
+
+n = int(input("Enter Number: "))
+
+for i in range(1, n + 1):           # loop for rows
+    print(" " * (n-i), end="")      # printing spaces
+
+    for j in range(1, 2 * i):
+        print(j, end="")            # printing digits
+    print()
+
+
+# practice
+
+n = int(input("Enter Number: "))
+
+for i in range(1, n+1):              # loop for rows
+    print(" " * (n-i), end="")       # printing spaces
+
+    for j in range(1 , 2 * i):
+        print(chr(64 + j), end="")   # printing characters
+    print()
