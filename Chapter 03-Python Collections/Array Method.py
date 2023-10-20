5271a08938d2370262124c76244c9e1bb8f1db59
Mirza Abdullah Baig <abaig7921gmail.com>
2023-10-20 11:06:01 +0500

    Array Methods

--- Chapter 03-Python Collections/Array Method.py
+++ Chapter 03-Python Collections/Array Method.py
@@ -1,44 +1,44 @@
-# Adding items using append, insert and extend methods
+# # Adding items using append, insert and extend methods
 
-fruits = ["Apple", "Banana", "Mango", "Cherry"]
+# fruits = ["Apple", "Banana", "Mango", "Cherry"]
 
-fruits.append("orange")  # Using append to add itmes to end of the list
-fruits.insert(3, "Amrrod")  # Using insert to add itmes to index of the list
+# fruits.append("orange")  # Using append to add itmes to end of the list
+# fruits.insert(3, "Amrrod")  # Using insert to add itmes to index of the list
 
-new_fruits = ["Graps", "Lemon"]
+# new_fruits = ["Graps", "Lemon"]
 
-fruits.extend(new_fruits) # Using extend to add new list to another list
+# fruits.extend(new_fruits) # Using extend to add new list to another list
 
-print(fruits)
-
-
-# Removing items using remove and pop method
-
-fruits.remove("Amrrod") # Using remove to remove specified items from list
-fruits.pop(2) # Using pop to remove items at giving index or else last iten
-fruits.pop()
-
-print(fruits)
+# print(fruits)
 
 
-#  Changing items using index number
+# # Removing items using remove and pop method
 
-fruits[2] = "Dates"
-fruits[3:5] = ["Amrrod", "Graps"]
-print(fruits)
+# fruits.remove("Amrrod") # Using remove to remove specified items from list
+# fruits.pop(2) # Using pop to remove items at giving index or else last iten
+# fruits.pop()
 
-# Sorting items using sort and reverse methods
+# print(fruits)
 
-numbers = [60, 20, 30, 80, 50, 70, 40, 10]
 
-numbers.sort() # By default ascending
-print(numbers)
+# #  Changing items using index number
 
-numbers.sort(reverse=True) # Descending
-print(numbers)
+# fruits[2] = "Dates"
+# fruits[3:5] = ["Amrrod", "Graps"]
+# print(fruits)
 
-numbers.reverse() # Reseverse
-print(numbers)
+# # Sorting items using sort and reverse methods
+
+# numbers = [60, 20, 30, 80, 50, 70, 40, 10]
+
+# numbers.sort() # By default ascending
+# print(numbers)
+
+# numbers.sort(reverse=True) # Descending
+# print(numbers)
+
+# numbers.reverse() # Reseverse
+# print(numbers)
 
 # List Comprehension 
 
@@ -50,3 +50,9 @@ A_students = [i for i in students if "A" in i]
 
 print(highest_mark)
 print(A_students)
+
+# Copy and Concatenate Array using .copy method
+
+mark = marks.copy() # copy array
+All_marks = mark + highest_mark # Concatenate Array
+print(All_marks)
